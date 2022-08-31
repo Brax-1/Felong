3264687e3d1e334209d82480677ca96f7716daed
Brax-1 <ay701548@gmail.com>
2022-08-31 16:18:39 +0530

    google auth integrated

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
-import { Route, Routes } from 'react-router-dom';
+import { getAuth } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import { Home, Login } from './components';
+import { app } from './config/firebase.config';
 
 function App() {
+  const firebaseAuth = getAuth(app);
+  const navigate = useNavigate();
+  const [auth,setAuth] = useState(false || window.localStorage.getItem("felongAuth")==="true")
+  useEffect(() => {
+    firebaseAuth.onAuthStateChanged((userCred)=>{
+      console.log(userCred)
+      if(userCred){
+        userCred.getIdToken().then((token)=>{
+          console.log(token);
+        })
+      }
+      else{
+        setAuth(false);
+        window.localStorage.setItem("auth","false");
+        navigate("/login");
+      }
+    })
+  }, [])
+  
   return (
     <div className="w-screen h-screen bg-primary">
       <Routes>
-        <Route path='/login' element={<Login/>}/>
+        <Route path='/login' element={<Login setAuth={setAuth}/>}/>
         <Route path='*' element={<Home/>}/>
       </Routes>
     </div>
--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,19 +1,57 @@
-import React from 'react'
-import {FcGoogle} from 'react-icons/fc'
+import React, { useEffect } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { app } from "../config/firebase.config";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { getApps } from "firebase/app";
 
-const Login = () => {
-  return (
-    <div className='relative w-screen h-screen'>
-        <div className='absolute inset-0 bg-darkOverlay flex items-center justify-center p-4'>
-            <div className='w-full h-fit md:w-375 p-4 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md flex flex-col items-center justify-center'>
-                <div className='flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-cardOverlay cursor-pointer hover:bg-white hover:shadow-md duration-50 ease-in-out transition-all'>
-                    <FcGoogle className='text-xl'/>
-                    Sign in with google
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+const Login = (props) => {
+	const firebaseAuth = getAuth(app);
+	const provider = new GoogleAuthProvider();
 
-export default Login
+	const navigate = useNavigate();
+
+	const loginWithGoogle = async () => {
+		await signInWithPopup(firebaseAuth, provider).then((res) => {
+			if (res) {
+				props.setAuth(true);
+				window.localStorage.setItem("felongAuth", "true");
+				firebaseAuth.onAuthStateChanged((userCred) => {
+					if (userCred) {
+						userCred.getIdToken().then((token) => {
+							console.log(token);
+						});
+						// navigate("/", { replace: true });
+					} else {
+						props.setAuth(false);
+						window.localStorage.setItem("auth", "false");
+						navigate("/login");
+					}
+				});
+			}
+		});
+	};
+	useEffect(() => {
+		if (window.localStorage.getItem("felongAuth") === "true") {
+			navigate("/", { replace: true });
+		}
+	}, []);
+
+	return (
+		<div className="relative w-screen h-screen">
+			<div className="absolute inset-0 bg-darkOverlay flex items-center justify-center p-4">
+				<div className="w-full h-fit md:w-375 p-4 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md flex flex-col items-center justify-center">
+					<div
+						className="flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-cardOverlay cursor-pointer hover:bg-white hover:shadow-md duration-50 ease-in-out transition-all"
+						onClick={loginWithGoogle}
+					>
+						<FcGoogle className="text-xl" />
+						Sign in with google
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
